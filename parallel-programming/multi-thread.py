47481a94ea57b5fae366270805422a258a60d9ae
정예슬 <68930784+dptmf35@users.noreply.github.com>
2022-06-24 09:30:38 +0900

    multi-thread

--- parallel-programming/multi-thread.py
+++ parallel-programming/multi-thread.py
@@ -24,6 +24,16 @@ if __name__ == '__main__' :
     for thread in threads :
         thread.join() # 실행 완료된 쓰레드를 합침
 
+    # threads = [None] * 10
+    #
+    # for i in range(10) : # 반복 스텝마다 프로세스 생성 및 실행
+    #     threads[i] = threading.Thread(target=test, args=[i])
+    #     threads[i].start()
+    #
+    # for i in range(10) :
+    #     threads[i].join()
+
+
     print(f'Run Time : {time.time() - start :.4f}') # --> multi-thread
 
 
